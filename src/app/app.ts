83081f9b56ab5b4fca39bb6d7c03a8423360acfa
Elena Novillo Luceño <enovilloluceno@deloittedigital.es>
2026-01-17 16:20:50 +0100

    add(MilestonesBanner): create new component

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { Header } from './components/header/header';
 import { MainBanner } from './components/main-banner/main-banner';
 import { SkillsBanner } from "./components/skills-banner/skills-banner";
+import { MilestonesBanner } from './components/milestones-banner/milestones-banner';
 
 @Component({
   selector: 'app-root',
-  imports: [Header, MainBanner, SkillsBanner],
+  imports: [Header, MainBanner, SkillsBanner, MilestonesBanner],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
