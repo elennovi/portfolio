da3ad60f5ea4b661b6dc54afeb2c31c0836d62fa
Elena Novillo Luceño <enovilloluceno@deloittedigital.es>
2026-01-18 21:36:38 +0100

    feat(Header): add redirection to components

--- src/app/components/header/header.ts
+++ src/app/components/header/header.ts
@@ -1,26 +1,11 @@
 import { Component } from '@angular/core';
 import { Link } from '../core/link/link';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [Link],
+  imports: [Link, RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class Header {
-  onSkillsClicked(): void {
-    console.log('Skills!');
-  }
-  onMilestonesClicked(): void {
-    console.log('Milestones!');
-  }
-  onSoftSkillsClicked(): void {
-    console.log('Soft Skills!');
-  }
-  onTrajectoryClicked(): void {
-    console.log('Trajectory!');
-  }
-  onContactClicked(): void {
-    console.log('Contact!');
-  }
-}
+export class Header {}
